bf43f1248aa9eb4db708044c937928968f060052
cheryl.s.kouadio <cheryl.s.kouadio@gmail.com>
2023-04-17 01:09:11 +0200

    Ajout de 'Tower of God', ' Tétris', et 'Itaewon class' sur la page index.html AJout d'un background-color:whitesmoke sur les items 'card' Mise à droite les items 'A propos' et 'Nous ecrire' dans le menu

---
+++ Pages/mail.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
+    <?php
+    $retour = mail('cheryl.s.kouadio@gmail.com', 'Envoi depuis la page Contact', $_POST['msg'], 'From'.$_POST['mail']);
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+    ?>
+</body>
+</html>
