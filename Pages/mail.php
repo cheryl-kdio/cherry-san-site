caddccff28561f33189436f9cfa546a353e47216
cheryl.s.kouadio <cheryl.s.kouadio@gmail.com>
2023-04-22 12:28:49 +0200

    Création de nouvelles pages Création des pages anime, films,musiques,litteratures Remplissage de la page anime ajout d'un style 'flex-container' et 'scrollbar' Modification de la page 'index.html' avec nouvelle disposition des card >>display: flex, class:flex container

--- Pages/mail.php
+++ Pages/mail.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8">
     <title>Envoi d'un message par formulaire</title>
@@ -7,9 +8,10 @@
 
 <body>
     <?php
-    $retour = mail('cheryl.s.kouadio@gmail.com', 'Envoi depuis la page Contact', $_POST['msg'], 'From'.$_POST['mail']);
+    $retour = mail('cheryl.s.kouadio@gmail.com', 'Envoi depuis la page Contact', $_POST['msg'], 'From' . $_POST['mail']);
     if ($retour)
         echo '<p>Votre message a bien été envoyé.</p>';
     ?>
 </body>
+
 </html>
